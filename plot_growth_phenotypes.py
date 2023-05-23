72bf79629669f3184ec8b99b29dad4fa61f9a0c9
Helen G Scott <helengracescott@gmail.com>
2023-05-23 09:34:02 -0400

    Clean up plot

--- plot_growth_phenotypes.py
+++ plot_growth_phenotypes.py
@@ -14,12 +14,20 @@ growth_phenotypes = pd.read_csv('known_growth_phenotypes_w_pred.tsv', sep='\t')
 growth_phenotypes = growth_phenotypes.set_index('c_source')
 growth_phenotypes = growth_phenotypes[['exp_growth', 'pred_growth']]
 
+# Rename the columns and the index to be longer/more descriptive
+growth_phenotypes.index.name = 'Carbon Source'
+growth_phenotypes = growth_phenotypes.rename(columns={'exp_growth': 'Experimental Growth',
+                                                      'pred_growth': 'FBA Predicted Growth'})
+
+# Replace all of the "No Exchange" values with "No"
+growth_phenotypes = growth_phenotypes.replace('No Exchange', 'No')
+
 # Make a dictionary for the phenotypes to numbers
-value_to_int = {'No': 0, 'Yes': 1, 'Unsure': 2, 'No Exchange': 3}
+value_to_int = {'Unsure': 0, 'No': 1, 'Yes': 2}
 n = len(value_to_int)
 
-# Make a colormap of specified colors
-cmap = ['red', 'green', 'yellow', 'gray']
+# Make a colormap of specified colors (in numerical order for the phenotypes)
+cmap = ['gray', '#F18F01', '#399E5A']
 
 # Plot the heatmap
 ax = sns.heatmap(growth_phenotypes.replace(value_to_int),
@@ -32,6 +40,18 @@ colorbar = ax.collections[0].colorbar
 r = colorbar.vmax - colorbar.vmin 
 colorbar.set_ticks([colorbar.vmin + r / n * (0.5 + i) for i in range(n)])
 colorbar.set_ticklabels(list(value_to_int.keys()))
+
+# Move the x-axis labels to the top
+plt.tick_params(axis='both',
+                which='major',
+                labelsize=10,
+                labelbottom = False,
+                bottom=False,
+                top = False,
+                labeltop=True)
+
+# Make sure that the y-axis labels are not cut off
+plt.tight_layout()
 
 # Save the figure
 plt.savefig('exp_vs_pred_growth_phenotypes.png')
