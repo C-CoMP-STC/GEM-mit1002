143372b9961b213e041cc964b2b23ab6a2b514e1
Helen G Scott <helengracescott@gmail.com>
2023-05-24 20:34:40 -0400

    Code for plotting venn diagram

--- kbase_models/pangenome_vs_union.py
+++ kbase_models/pangenome_vs_union.py
@@ -8,9 +8,13 @@ gem_bayesian_core = cobra.io.load_json_model('kbase_models/alteromonas_bayesian_
  
 # Second way
 venn2([set([r.id[0:-3] for r in gem_union.reactions]),
-       set([r.id for r in gem_bayesian_core.reactions])])
+       set([r.id for r in gem_bayesian_core.reactions])],
+       set_labels=('Union of All \n KBase Models',
+                   'Pangenome \n Bayesian \n Core Model')
+       )
 
 # Style
+plt.tight_layout()  # This alone doesn't keep labels from hanging off
 
-# Save
-print("foo")
+# Save the figure
+plt.savefig('kbase_models/pangenome_vs_union.png')
