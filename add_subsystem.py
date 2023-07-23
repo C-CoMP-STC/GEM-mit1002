b1630b077d7d20935dfe24e735105608c5bd6e80
Helen G Scott <helengracescott@gmail.com>
2023-07-23 13:43:32 -0400

    Add subsystem fields

---
+++ add_subsystem.py
@@ -0,0 +1,13 @@
+import json
+
+# Open the model json file
+with open('model.json', 'r') as f:
+    model = json.load(f)
+
+# Loop through all of the reactions
+for reaction in model['reactions']:
+    reaction['subsystem'] = "TBD"
+
+# Save the model file
+with open('model.json', 'w') as f:
+    json.dump(model, f, indent=4)
