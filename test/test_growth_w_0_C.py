7f74f4c7a0a3f5d43aba9fed3e8b1477f6339b8c
Helen G Scott <helengracescott@gmail.com>
2023-07-24 17:24:17 -0400

    Change path to model file

--- test/test_growth_w_0_C.py
+++ test/test_growth_w_0_C.py
@@ -5,7 +5,7 @@ import cobra
 class TestGrowthWithNoCarbon(unittest.TestCase):
     def test_growth_w_0_C(self):
         # Load the model with cobrapy
-        model = cobra.io.read_sbml_model('../model.xml')
+        model = cobra.io.read_sbml_model('model.xml')
 
         # Set the media so that there are no carbon sources
         medium = {'EX_co2_e': 1000.0,
