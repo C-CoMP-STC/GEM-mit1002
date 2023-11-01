f3adfc77e6b48327f48a2c771066e09c28ad3b64
Helen G Scott <helengracescott@gmail.com>
2023-11-01 13:52:30 -0400

    Generate HTML report 10 times

--- repeat_cycle_finder.py
+++ repeat_cycle_finder.py
@@ -1,18 +1,22 @@
-import cobra
-import memote.support.consistency as consistency
+import subprocess
 
-# Load the model
-model = cobra.io.read_sbml_model("model.xml")
-
-met = "MNXM3"  # ATP
-
-print("Finding ATP generating cycles")
-print("----------------------------")
-# For a specfied number of times
+# Loop the number of times you want to run memote
 for i in range(0, 10):
-    # Make a copy of the model
-    model_copy = model.copy()
-    # Run just the function for finding ATP generating cycles
-    rxns = consistency.detect_energy_generating_cycles(model_copy, met)
-    # Print the results
-    print(rxns)
+    # Define the output file name
+    output_file = "report_" + str(i) + ".html"
+
+    # Define the command as a list of strings
+    command = [
+        "memote",
+        "report",
+        "snapshot",
+        "--filename",
+        output_file,
+        "model.xml",
+    ]
+
+    # Run the command using subprocess
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+    # Wait for the process to finish
+    stdout, stderr = process.communicate()
