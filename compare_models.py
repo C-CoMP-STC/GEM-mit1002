a181565ac9e200375bf4b84abe797a2d16b6bc60
Helen G Scott <helengracescott@gmail.com>
2024-04-19 15:23:02 -0400

    Make 1 table with all reaction info and presence/absence

--- compare_models.py
+++ compare_models.py
@@ -1,4 +1,5 @@
 import cobra
+import pandas as pd
 
 # Load the two models
 modelseedpy_model = cobra.io.read_sbml_model("modelseedpy_model.xml")
@@ -9,26 +10,39 @@ modelseedpy_reactions = set([reaction.id for reaction in modelseedpy_model.react
 kbase_reactions = set([reaction.id for reaction in kbase_model.reactions])
 union_reactions = modelseedpy_reactions.union(kbase_reactions)
 
-# Make a table of the union reactions with the reacion ID,
+# Make a pandas dataframe of the union reactions with the reacion ID,
 # if the reaction is present in each model, the reaction name,
 # and the reaction equation
-with open("union_reactions.tsv", "w") as f:
-    f.write("ID\tKBase Model\tModelSEEDpy Model\tName\tEquation\n")
-    for reaction_id in union_reactions:
-        f.write(f"{reaction_id}\t")
-        # Check if the reaction is in each model
-        if reaction_id in kbase_reactions:
-            f.write("Y\t")
-        else:
-            f.write("\t")
-        if reaction_id in modelseedpy_reactions:
-            f.write("Y\t")
-        else:
-            f.write("\t")
-        # Get the reaction name and equation
-        if reaction_id in modelseedpy_reactions:
-            reaction = modelseedpy_model.reactions.get_by_id(reaction_id)
-            f.write(f"{reaction.id}\t{reaction.name}\t{reaction.reaction}\n")
-        else:
-            reaction = kbase_model.reactions.get_by_id(reaction_id)
-            f.write(f"{reaction.id}\t{reaction.name}\t{reaction.reaction}\n")
+reaction_data = []
+for reaction_id in union_reactions:
+    row = {"ID": reaction_id}
+    # Check if the reaction is in each model
+    if reaction_id in kbase_reactions:
+        row["KBase Model"] = "Y"
+    else:
+        row["KBase Model"] = "N"
+    if reaction_id in modelseedpy_reactions:
+        row["ModelSEEDpy Model"] = "Y"
+    else:
+        row["ModelSEEDpy Model"] = "N"
+    # Get the reaction name and equation
+    if reaction_id in modelseedpy_reactions:
+        reaction = modelseedpy_model.reactions.get_by_id(reaction_id)
+        row["Name"] = reaction.name
+        row["Equation"] = reaction.reaction
+    else:
+        reaction = kbase_model.reactions.get_by_id(reaction_id)
+        row["Name"] = reaction.name
+        row["Equation"] = reaction.reaction
+    reaction_data.append(row)
+
+# Sort the list, so that the reactions only in the KBase model are
+# at the top, then the reactions only in the ModelSEEDpy model, then the
+# reactions in both models
+reaction_df = pd.DataFrame(reaction_data)
+reaction_df = reaction_df.sort_values(
+    by=["ModelSEEDpy Model", "KBase Model"], ascending=[True, True]
+)
+
+# Save the dataframe to a CSV file
+reaction_df.to_csv("model_comparison.csv", index=False)
