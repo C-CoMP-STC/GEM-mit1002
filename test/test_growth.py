ef8d9f30375061b80ec1d09441852ca07225830d
Helen G Scott <helengracescott@gmail.com>
2024-06-07 14:21:10 -0400

    Get the modelseed IDs

--- test/test_growth.py
+++ test/test_growth.py
@@ -38,7 +38,34 @@ ZAC = {
 }
 
 # L1 Minimal Media
-L1 = {}
+# A general purpose marine medium for growing coastal algae
+# An enriched seawater medium, with everything added to filtered natural seawater
+# Does that mean there are other carbon/nitrogen sources in the media?
+L1 = {"EX_cpd00007_e0": 20,  # O2_e0
+      # L1 salts
+      "EX_cpd00971_e0": 1000,  # Na+_e0 (in NaNO3, NaH2PO4, NaSiO3, Na2EDTA, NaMoO4, Na3VO4)
+      "EX_cpd00009_e0": 1000,  # Phosphate (HO4P) (in NaH2PO4)
+      "EX_cpd20826_e0": 1000,  # Silica (O2Si) (in NaSiO3)
+      # Trace element solution
+      "EX_cpd00240_e0": 1000,  # EDTA (in Na2EDTA)
+      "EX_cpd10516_e0": 1000,  # fe3_e0 (in FeCl3)
+      "EX_cpd00099_e0": 1000,  # Cl- (in FeCl3, MnCl2, CoCl2)
+      "EX_cpd00030_e0": 1000,  # Mn2+ (in MnCl2)
+      "EX_cpd00034_e0": 1000,  # Zn2+ (in ZnSO4)
+      "EX_cpd00048_e0": 1000,  # Sulfate (O4S) (in ZnSO4, CuSO4, NiSO4)
+      "EX_cpd00149_e0": 1000,  # Co2+ (in CoCl2)
+      "EX_cpd00058_e0": 1000,  # Cu2+_e0 (in CuSO4)
+      "EX_cpd11574_e0": 1000,  # Molybdate (MoO4) (in NaMoO4)
+      "EX_cpd03387_e0": 1000,  # Selenite (O3Se) (in H2SeO3)
+      "EX_cpd00244_e0": 1000,  # Ni2+ (in NiSO4)
+      "EX_cpd08438_e0": 1000,  # Ortho-vanadate (H2O4V) (in Na3VO4)
+      "EX_cpd00205_e0": 1000,  # K+ (in K2CrO4)
+      "EX_cpd11595_e0": 1000,  # Chromate (H2CrO4) (in K2CrO4)
+      # Vitamin solution
+      "EX_cpd00305_e0": 1000,  # Thiamine HCl (Vitamin B1)
+      "EX_cpd00104_e0": 1000,  # Biotin (Vitamin H)
+      "EX_cpd01826_e0": 1000,  # Cyanocobalamin (Vitamin B12)
+      }
 
 
 class TestGrowthPhenotypes(unittest.TestCase):
