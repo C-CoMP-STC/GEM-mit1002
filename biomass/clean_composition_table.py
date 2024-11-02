ac70ba88a7e02adcf6b906c3dcefd0ed69e5e672
Helen G Scott <helengracescott@gmail.com>
2024-11-02 14:56:00 -0400

    Add metabolite groups

--- biomass/clean_composition_table.py
+++ biomass/clean_composition_table.py
@@ -30,6 +30,41 @@ df_melted.dropna(subset=['Metabolite'], inplace=True)
 # Create a pivot table where each column is a model and each row is a metabolite, indicating presence (1) or absence (0)
 df_pivot = pd.pivot_table(df_melted, index='Metabolite', columns='Model', aggfunc='size', fill_value=0)
 
+# Load the excel sheet with the metabolite groups and order I want to use
+metabolite_groups = pd.read_excel(input_path, sheet_name="4.Nomenclature_mapping", skiprows=1)
+
+# Split the table into dataframes for each group by taking the rows between the group names
+groups = ["PROTEIN", "tRNA", "mRNA and DNA", "LIPIDS", "COFACTORS & PROSTHETIC GROUPS", "CARBOHYDRATES, CELL-WALL and OTHERS"]
+dfs = []
+for i, group in enumerate(groups):
+    # Get the index of where that group will start in the table (including the row with the group name)
+    start_row = metabolite_groups.index[metabolite_groups['This work'] == group].tolist()[0]
+    if i < len(groups) - 1:
+        # Get the index of where the next group will start in the table (including the row with the group name)
+        end_row = metabolite_groups.index[metabolite_groups['This work'] == groups[i + 1]].tolist()[0]
+        # Get the rows between the group names
+        df_group = metabolite_groups.loc[start_row+1:end_row-1]
+    else:
+        # For the last group, the group will end at the end of the table
+        end_row = metabolite_groups.shape[0]
+        # Get the rows between the group name and the end of the table
+        df_group = metabolite_groups.loc[start_row+1:end_row]
+    # Add a column with the group name
+    df_group['Group'] = group
+    # Get rid of all the columns other than "This work" and "Group", and rename "This work" to "Metabolite"
+    df_group = df_group[['This work', 'Group']].rename(columns={'This work': 'Metabolite'})
+    # Add to the list of dataframes
+    dfs.append(df_group)
+
+# Merge all of the dataframes into a single dataframe
+group_metabolite_ids = pd.concat(dfs)
+
+# Add a row called 'Group' to the pivot table, which will contain the metabolite group for each metabolite
+df_pivot['Group'] = df_pivot.index.map(group_metabolite_ids.set_index('Metabolite')['Group'])
+
+# Reorder to match the order of the metabolite groups
+df_pivot = df_pivot.sort_values('Group')
+
 # Save the result to a CSV file
 output_path = os.path.join(OUT_DIR, 'standardized_compositions.csv')
 df_pivot.to_csv(output_path)
