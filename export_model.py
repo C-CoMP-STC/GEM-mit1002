55621e8862144561d2c93bc22fc917e579b2e648
Helen G Scott <helengracescott@gmail.com>
2025-01-13 10:50:49 -0500

    Export to excel

--- export_model.py
+++ export_model.py
@@ -1,7 +1,24 @@
 import cobra
+import json
+import pandas as pd
 
 # Load the model from the SBML file
 model = cobra.io.read_sbml_model('model.xml')
 
 # Export the model to JSON
 cobra.io.save_json_model(model, 'model.json')
+
+# Convert to excel file
+# Load the json model 
+with open('model.json') as f:
+    model_json = json.load(f)
+
+met_df = pd.DataFrame(model_json['metabolites'])
+rxn_df = pd.DataFrame(model_json['reactions'])
+gene_df = pd.DataFrame(model_json['genes'])
+
+# Save to excel
+with pd.ExcelWriter('model.xlsx') as writer:
+    met_df.to_excel(writer, sheet_name='metabolites', index=False)
+    rxn_df.to_excel(writer, sheet_name='reactions', index=False)
+    gene_df.to_excel(writer, sheet_name='genes', index=False)
