ce4f625b27f51dd5d3acd14bde627c5822882da6
Helen G Scott <helengracescott@gmail.com>
2025-01-24 16:28:43 -0500

    Add TODO and lint

--- export_model.py
+++ export_model.py
@@ -1,34 +1,35 @@
-import cobra
 import json
+
+import cobra
 import pandas as pd
 
 # Load the model from the SBML file
-model = cobra.io.read_sbml_model('model.xml')
+model = cobra.io.read_sbml_model("model.xml")
 
 # Export the model to JSON
-cobra.io.save_json_model(model, 'model.json')
+cobra.io.save_json_model(model, "model.json")
 
 # Convert to excel file
-# Load the json model 
-with open('model.json') as f:
+# Load the json model
+with open("model.json") as f:
     model_json = json.load(f)
 
 # Make pandas data frames for the three main components of the model
-met_df = pd.DataFrame(model_json['metabolites'])
-rxn_df = pd.DataFrame(model_json['reactions'])
-gene_df = pd.DataFrame(model_json['genes'])
+met_df = pd.DataFrame(model_json["metabolites"])
+rxn_df = pd.DataFrame(model_json["reactions"])
+gene_df = pd.DataFrame(model_json["genes"])
 
 
 # Write a function to go from the stoichiometry dictionary to a string of the reaction with human-friendly metabolite names
 def build_reaction_string(stoichiometry_dict):
-    reactants_side = ' + '.join(
+    reactants_side = " + ".join(
         [
             f"{abs(coeff)} {met_df.loc[met_df['id'] == met_id, 'name'].values[0]}"
             for met_id, coeff in stoichiometry_dict.items()
             if coeff < 0
-        ]
+        ]  # TODO: Change this into another function, so I can reuse it below
     )
-    products_side = ' + '.join(
+    products_side = " + ".join(
         [
             f"{abs(coeff)} {met_df.loc[met_df['id'] == met_id, 'name'].values[0]}"
             for met_id, coeff in stoichiometry_dict.items()
@@ -37,13 +38,17 @@ def build_reaction_string(stoichiometry_dict):
     )
     return f"{reactants_side} -> {products_side}"
 
+
 # Apply the function to the reactions data frame and add a column for the reaction string
-rxn_df['reaction'] = rxn_df['metabolites'].apply(build_reaction_string)
+rxn_df["reaction"] = rxn_df["metabolites"].apply(build_reaction_string)
 # Place the column third, after id and name, then keep the rest of the order the same
-rxn_df = rxn_df[['id', 'name', 'reaction'] + [col for col in rxn_df.columns if col not in ['id', 'name', 'reaction']]]
+rxn_df = rxn_df[
+    ["id", "name", "reaction"]
+    + [col for col in rxn_df.columns if col not in ["id", "name", "reaction"]]
+]
 
 # Save to excel
-with pd.ExcelWriter('model.xlsx') as writer:
-    met_df.to_excel(writer, sheet_name='metabolites', index=False)
-    rxn_df.to_excel(writer, sheet_name='reactions', index=False)
-    gene_df.to_excel(writer, sheet_name='genes', index=False)
+with pd.ExcelWriter("model.xlsx") as writer:
+    met_df.to_excel(writer, sheet_name="metabolites", index=False)
+    rxn_df.to_excel(writer, sheet_name="reactions", index=False)
+    gene_df.to_excel(writer, sheet_name="genes", index=False)
