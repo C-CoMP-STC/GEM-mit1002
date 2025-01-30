c5daaaf262075947cae2de5ea90f97b0b37c1e32
Helen Scott <helengracescott@gmail.com>
2025-01-30 14:59:42 -0500

    Try to run CarveMe on real results

--- make_carveme_model.sh
+++ make_carveme_model.sh
@@ -1,4 +1,9 @@
 #!/bin/bash -l
+#$ -P cometsfba
+#$ -N carveme
+#$ -pe omp 16
+#$ -o make_carveme_model.out
+#$ -j y  # Merge the error and output streams into a single file
 
 module load python3/3.6.12
 module load cplex
@@ -6,4 +11,9 @@ module load diamond
 module load carveme/1.5.1
 
 # Run CarveMe
-carve --egg eggnog_output.emapper.annotations -g LB --fbc2
+carve --egg genome/eggnog_output.emapper.annotations \
+    -g mbm_glc__D,l1_glc__D,mbm_ac,l1_ac,mbm_ala__L,l1_ala__L,mbm_pro__L,l1_pro__L,l1_glyc3p,l1_tyr__L,l1_glu__L,l1_val__L \
+    --mediadb test/test_files/media/media_database.tsv \
+    --fbc2 \
+    -o carveme_model.xml \
+    -v
