33454059ef34f83af11a527b22372014827a83c4
Helen G Scott <helengracescott@gmail.com>
2025-03-27 11:03:31 -0400

    fix: call ko annotations correctly

--- plot_kegg_maps.py
+++ plot_kegg_maps.py
@@ -6,14 +6,16 @@ from gem_utilities.maps import map_ko_ids
 FILE_DIR = os.path.dirname(os.path.abspath(__file__))
 OUT_DIR = os.path.join(FILE_DIR, 'kegg_maps')
 
+# If the outpath doesn't exist, create it
+os.makedirs(OUT_DIR, exist_ok=True)
+
 # Load the model
 model = cobra.io.read_sbml_model(os.path.join(FILE_DIR, 'model.xml'))
 
 # Extract the KO IDs from the model
-# FIXME: Right now there are no KO IDs in the model
 ko_ids = set()
 for reaction in model.reactions:
-    for ko_id in reaction.annotation.get('kegg.ko', []):
+    for ko_id in reaction.annotation.get('kegg.orthology', []):
         ko_ids.add(ko_id)
 
 # Make and save a single map
@@ -23,4 +25,3 @@ map_ko_ids("ko00061",
            ko_ids,
            kgml_folder='/Users/helenscott/Documents/PhD/Segre-lab/kegg_data/kgml',
            output_folder=OUT_DIR)
-
