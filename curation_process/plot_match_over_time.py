99e06e67cd42b4287e027d825a2ba4548dd85c51
Helen G Scott <helengracescott@gmail.com>
2025-10-22 10:24:59 -0400

    feat: Use PR numbers on x axis

--- curation_process/plot_match_over_time.py
+++ curation_process/plot_match_over_time.py
@@ -22,20 +22,23 @@ data = pd.read_csv(os.path.join(FILE_DIR, "growth_match_summary.csv"))
 # Skip rows that say "Error" in the % Match column
 data = data[data["% Match"] != "ERROR"]
 
-# Convert the % Match column to numeric
+# Convert the % Match and the PR number columns to numeric
 data["% Match"] = pd.to_numeric(data["% Match"])
+data["PR Number"] = pd.to_numeric(data["PR Number"])
 
 # Plot the percentage of matches
 plt.figure(figsize=(10, 6))
 plt.plot(
-    range(len(data)),
+    data["PR Number"],
     data["% Match"],
     marker="o",
     linestyle="-",
     color="blue",
 )
 plt.title("Percentage of Matches Over Time")
-plt.xlabel("Pull Requests")
+plt.xlabel("Pull Request Number")
+# Make sure x ticks are integers
+plt.xticks(data["PR Number"].unique())
 plt.ylabel("Percentage Match (%)")
 
 # Save the plot
