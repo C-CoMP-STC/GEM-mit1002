ac31e0e83454e09742b0af34ad75aab026ae18fd
Helen G Scott <helengracescott@gmail.com>
2025-11-05 10:42:11 -0500

    fix: reimplement plotting changes for ace/leu/ile

--- curation_process/plot_match_over_time.py
+++ curation_process/plot_match_over_time.py
@@ -11,23 +11,34 @@ data = pd.read_csv(os.path.join(FILE_DIR, "growth_match_summary.csv"))
 # Loop through the rows, if the row does not have a different number of
 # reactions, metabolites, or genes (i.e. if the model has not changed),
 # remove it
-data = data[
-    (data["Reactions"] != data["Reactions"].shift(1))
-    | (data["Metabolites"] != data["Metabolites"].shift(1))
-    | (data["Genes"] != data["Genes"].shift(1))
-]
+# data = data[
+#     (data["Reactions"] != data["Reactions"].shift(1))
+#     | (data["Metabolites"] != data["Metabolites"].shift(1))
+#     | (data["Genes"] != data["Genes"].shift(1))
+# ]
+
+# Skip rows that say "Error" in the % Match column
+data = data[data["% Match"] != "ERROR"]
+
+# Convert the % Match and the PR number columns to numeric
+data["% Match"] = pd.to_numeric(data["% Match"])
+data["PR Number"] = pd.to_numeric(data["PR Number"])
 
 # Plot the percentage of matches
 plt.figure(figsize=(10, 6))
 plt.plot(
-    range(len(data)),
+    data["PR Number"],
     data["% Match"],
     marker="o",
     linestyle="-",
     color="blue",
 )
 plt.title("Percentage of Matches Over Time")
-plt.xlabel("Pull Requests")
+plt.xlabel("Pull Request Number")
+# Make sure x ticks are integers
+plt.xticks(data["PR Number"].unique())
+# Turn x labels by 45 degrees
+plt.xticks(rotation=45)
 plt.ylabel("Percentage Match (%)")
 
 # Save the plot
