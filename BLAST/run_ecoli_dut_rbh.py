8256d528070913ecd65e390e92b67bd08adbaa99
Helen G Scott <helengracescott@gmail.com>
2025-12-12 13:40:59 -0500

    refactor: lint

--- BLAST/run_ecoli_dut_rbh.py
+++ BLAST/run_ecoli_dut_rbh.py
@@ -7,7 +7,9 @@ PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 final_results = find_reciprocal_best_hits(
     query_seq_file=os.path.join(PROJECT_ROOT, "BLAST/ecoli_dut_query/dut_seq.fa"),
-    organism_fasta=os.path.join(PROJECT_ROOT, "genome/Michelle_4106_gene_calls/MIT1002_anvio_prot_seqs.fa"),
+    organism_fasta=os.path.join(
+        PROJECT_ROOT, "genome/Michelle_4106_gene_calls/MIT1002_anvio_prot_seqs.fa"
+    ),
     organism_db=os.path.join(PROJECT_ROOT, "BLAST/dbs/amac_db"),
     reference_db=os.path.join(PROJECT_ROOT, "BLAST/dbs/ecoli_db"),
     output_dir=os.path.join(PROJECT_ROOT, "BLAST/ecoli_dut_query/results"),
