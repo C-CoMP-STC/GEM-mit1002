720439a6e979891455288f41d6ad389f45b2a686
Helen G Scott <helengracescott@gmail.com>
2025-12-12 13:44:29 -0500

    refactor: rename dir for better sorting

--- BLAST/run_ecoli_dut_rbh.py
+++ BLAST/run_ecoli_dut_rbh.py
@@ -6,12 +6,12 @@ from auto_rbh import find_reciprocal_best_hits
 PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 final_results = find_reciprocal_best_hits(
-    query_seq_file=os.path.join(PROJECT_ROOT, "BLAST/ecoli_dut_query/dut_seq.fa"),
+    query_seq_file=os.path.join(PROJECT_ROOT, "BLAST/query_ecoli_dut/dut_seq.fa"),
     organism_fasta=os.path.join(
         PROJECT_ROOT, "genome/Michelle_4106_gene_calls/MIT1002_anvio_prot_seqs.fa"
     ),
     organism_db=os.path.join(PROJECT_ROOT, "BLAST/dbs/amac_db"),
     reference_db=os.path.join(PROJECT_ROOT, "BLAST/dbs/ecoli_db"),
-    output_dir=os.path.join(PROJECT_ROOT, "BLAST/ecoli_dut_query/results"),
+    output_dir=os.path.join(PROJECT_ROOT, "BLAST/query_ecoli_dut/results"),
     num_forward_hits=10,
 )
